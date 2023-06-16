d726df66f68368ab5c42784033ff5b6481bc3b12
hgbaodev <120194990+hgbaodev@users.noreply.github.com>
2023-06-16 16:12:18 +0700

    loading movie search

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -15,6 +15,10 @@ const MoviePage = () => {
 
   const filterDebounce = useDebounce(filter, 1000);
 
+  const { data, error } = useSWR(url, fetcher);
+
+  const loading = !data && !error;
+
   useEffect(() => {
     if (filterDebounce) {
       setUrl(
@@ -24,8 +28,6 @@ const MoviePage = () => {
       setUrl(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`);
     }
   }, [filterDebounce]);
-
-  const { data } = useSWR(url, fetcher);
 
   const movies = data?.results || [];
 
@@ -57,11 +59,52 @@ const MoviePage = () => {
           </svg>
         </button>
       </div>
+      {loading && (
+        <div className="w-10 h-10 rounded-full  border-4 border-primary border-t-transparent border-t-4 mx-auto animate-spin"></div>
+      )}
       <div className="grid grid-cols-4 gap-10">
-        {movies.length > 0 &&
+        {!loading &&
+          movies.length > 0 &&
           movies.map((item) => (
             <MovieCard key={item.id} item={item}></MovieCard>
           ))}
+      </div>
+      <div className="flex items-center justify-center mt-10 gap-x-5">
+        <span className="cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+            />
+          </svg>
+        </span>
+        <span className="cursor-pointer inline-block py-2 px-4 leading-none rounded-lg bg-white text-slate-900">
+          1
+        </span>
+        <span className="cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+            />
+          </svg>
+        </span>
       </div>
     </div>
   );
